46f9213a9af84991b1c5b280620c3c763be1a861
Manish <jain.jainankit.ankit@gmail.com>
2014-03-29 10:20:17 +0530

    reporting bootstrap

---
+++ public/js/messages/analysis.js
@@ -0,0 +1,11 @@
+(function () {
+	'use strict';
+
+	define([], function () {
+		return {
+			serverError: 		'Server error',
+			loadingReport: 		'Loading Report'
+	  	};
+	});
+
+})();
--- public/js/views/reportsLoader.js
+++ public/js/views/reportsLoader.js
@@ -4,24 +4,60 @@
 	define([
 		'libs',
 		'utils',
+    'models/analysis/reportBootstrap',
 		'controllers/analysis/reportController',
 		'views/baseView',
-		'views/analysis/reportsSidebar'
-	], function (libs, utils, ReportController, BaseView, ReportsSidebar) {
+		'views/analysis/reportsSidebar',
+    'messages/analysis'
+	], function (libs, utils, ReportBootstrapModel, ReportController, BaseView, ReportsSidebar, messages) {
 
-		var Notification = utils.Notification,
-        Vent = utils.Vent;
+		var $ = libs.jquery,
+        Notification = utils.Notification,
+        Vent = utils.Vent,
+
+    fetchBootstrapData = function () {
+      var that = this,
+          deferred =  $.Deferred(),
+          reportBootstrapModel = new ReportBootstrapModel();
+
+      reportBootstrapModel.fetch({
+          success: function (response) {
+              deferred.resolve(response);
+          },
+          error: function () {
+              deferred.reject();
+          }
+      });
+
+      return deferred.promise();
+
+    },
+
+    loadReport = function () {
+      var that = this,
+          reportsWorkspace;
+
+      Notification.info(messages.loadingReport);
+      fetchBootstrapData.call(that)
+      .done(function(bootstrap) {
+          reportsWorkspace = new ReportController({
+          bootstrap: bootstrap
+        });
+        reportsWorkspace.render();
+      })
+      .fail(function () {
+        Notification.error(messages.serverError);
+      });
+    };
 
 		return BaseView.extend({
 			initialize: function (options) {
-          var that = this,
-              reportsWorkspace;
+          var that = this;
           
           BaseView.prototype.initialize.call(that, options);
 
           that.listenTo(Vent, 'analysis:sidebarLoaded', function () {
-            reportsWorkspace = new ReportController();
-            reportsWorkspace.render();
+            loadReport.call(that);
           });
 
           that.render();
